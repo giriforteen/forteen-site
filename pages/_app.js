8a84b27274fed210cdbda5cee69953954f6b2034
giriforteen <106420929+giriforteen@users.noreply.github.com>
2025-12-06 18:34:34 +0530

    Fix: Add UTF-8 charset declaration for emoji display

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Head from 'next/head'
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
