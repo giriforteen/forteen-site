9b79f437f8d0a6894eee1ae9d7b190cb2814593a
giriforteen <106420929+giriforteen@users.noreply.github.com>
2025-12-13 16:42:01 +0530

    Add episode redirects

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/episode2',
+        destination: 'https://giriforteen.github.io/unforgettable-forteen/unforgettable_episode2_before_you_speak.html',
+        permanent: false, // Use 307 temporary redirect (can change later)
+      },
+      {
+        source: '/pilot',
+        destination: 'https://giriforteen.github.io/unforgettable-forteen/presence_premium_pilot_v2.html',
+        permanent: false,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
